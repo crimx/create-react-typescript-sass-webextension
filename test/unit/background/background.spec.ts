39a84208456a0ad233ac50429549c774ce85b572
CRIMX <straybugs@gmail.com>
2018-01-25 19:12:15 +0800

    test(all): switch to sinon-chrome

--- test/unit/background/background.spec.ts
+++ test/unit/background/background.spec.ts
@@ -1,5 +1,16 @@
-import '../index'
+import '../../../src/background'
 
-it('should have called a webextension API', () => {
-  expect(browser.runtime.onMessage.addListener).toHaveBeenCalled()
+describe('background page', () => {
+  beforeAll(() => jest.useFakeTimers())
+  afterAll(() => jest.useRealTimers())
+
+  it('should set up a listener', () => {
+    expect(browser.runtime.onMessage.addListener.calledOnce).toBeTruthy()
+  })
+
+  it('should resopnse "GREETING" request', () => {
+    browser.runtime.onMessage.dispatch({ type: 'GREETING' })
+    expect(setTimeout).toHaveBeenCalledTimes(1)
+    expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000)
+  })
 })
--- test/unit/components/CompA.spec.tsx
+++ test/unit/components/CompA.spec.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import CompA from '../index'
+import CompA from '../../../src/components/CompA'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
